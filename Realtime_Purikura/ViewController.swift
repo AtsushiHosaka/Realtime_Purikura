1bd34dec10ba35056bae7c4123d126ed60002e24
Atsushi Hosaka <hosaka2917@gmail.com>
2024-06-19 00:35:48 +0900

    Initial Commit

---
+++ Realtime_Purikura/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Realtime_Purikura
+//
+//  Created by 保坂篤志 on 2024/06/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
